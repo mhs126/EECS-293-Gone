ef920bf4cc0c6f3eb810da05af32f5bd8fbb71f3
niharikakarnik <38048816+niharikakarnik@users.noreply.github.com>
2019-03-06 10:37:57 -0500

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,4 +1,3 @@
-package gone;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -7,72 +6,89 @@ import java.util.Map;
 
 public class Board {
 
-    private int height;
+	private int height;
 
-    private int width;
+	private int width;
 
-    private Map<String, Pebble> blackMap = new HashMap<>();
+	private Map<String, Pebble> blackMap = new HashMap<>();
 
-    private List<Pebble> whiteList = new ArrayList<>();
+	private List<Pebble> whiteList = new ArrayList<>();
 
-    public Board(int height, int width, List<Pebble> pebbleList){
-        this.height = height;
-        this.width = width;
-        this.sortByColor(pebbleList);
-    }
+	/*
+	 * @param: Height, Width, Pebble List
+	 * Constructor takes two ints and a
+	 * list of pebbles, which will indicate the width, height and board
+	 * configuration. Within the constructor the list of pebbles will be sorted
+	 * into the field variables of a white and black list
+	 */
+	public Board(int height, int width, List<Pebble> pebbleList) {
+		this.height = height;
+		this.width = width;
+		this.sortByColor(pebbleList);
+	}
 
-    public void sortByColor(List<Pebble> p){
-        for (Pebble pebble : p){
-            if(pebble.getColor() == Color.BLACK){
-                blackMap.put(pebble.toString(), pebble);
-            }
-            else{
-                whiteList.add(pebble);
-            }
-        }
-    }
+	/*
+	 * @param: List of pebbles 
+	 * sortPebblesByColor() accepts a list of pebbles and sorts the pebbles into their appropriate field variable
+	 * list, returns nothing.
+	 */
+	public void sortByColor(List<Pebble> p) {
+		for (Pebble pebble : p) {
+			if (pebble.getColor() == Color.BLACK) {
+				blackMap.put(pebble.toString(), pebble);
+			} else {
+				whiteList.add(pebble);
+			}
+		}
+	}
 
-    public int changeBlackPebbles(int steps) {
-        List<Pebble> changedPebbles = new ArrayList<>();
-        for (Pebble pebble : whiteList) {
-            for (String string : pebble.getNeighborsLocations()) {
-                Pebble p = blackMap.get(string);
-                if (p != null) {
-                    blackMap.remove(string);
-                    p.changeColorToWhite();
-                    changedPebbles.add(p);
-                }
-            }
-        }
-        if(changedPebbles.isEmpty()) {
-            return steps;
-        }
-        else{
-            whiteList.addAll(changedPebbles);
-            return this.changeBlackPebbles(steps + 1);
-        }
-    }
+	/* @param: counter
+	 * @return: counter
+	 * replaceWhiteAdjacentPebbles() Takes an integer of iterations as input, identifies 
+	 * where the white pebbles neighbors are, and looks for the neighbors in the blackmap to remove
+	 * and add to the white pebble list
+	 */
+	public int changeBlackPebbles(int steps) {
+		List<Pebble> changedPebbles = new ArrayList<>();
+		for (Pebble pebble : whiteList) {
+			for (String string : pebble.getNeighborsLocations()) {
+				Pebble p = blackMap.get(string);
+				if (p != null) {
+					blackMap.remove(string);
+					p.changeColorToWhite();
+					changedPebbles.add(p);
+				}
+			}
+		}
+		if (changedPebbles.isEmpty()) {
+			return steps;
+		} else {
+			whiteList.addAll(changedPebbles);
+			return this.changeBlackPebbles(steps + 1);
+		}
+	}
+	/*
+	 * @return a string stating the amount of turns took and the number of black pebbles remaining
+	 */
+	public String gameResult() {
+		return "It took " + this.changeBlackPebbles(0) + " steps to change the pebbles " + "and there are "
+				+ blackMap.size() + " black pebbles left";
+	}
 
-    public String gameResult(){
-        return "It took " + this.changeBlackPebbles(0) + " steps to change the pebbles " +
-                "and there are " + blackMap.size() + " black pebbles left";
-    }
-
-
-    public static void main(String[] args){
-        Pebble p = new Pebble(1,1, Color.WHITE);
-        Pebble p1 = new Pebble(1,2, Color.BLACK);
-        Pebble p2 = new Pebble(1,3, Color.BLACK);
-        Pebble p3 = new Pebble(1,0, Color.BLACK);
-        Pebble p4 = new Pebble(2,6, Color.BLACK);
-        List<Pebble> list = new ArrayList<>();
-        list.add(p);
-        list.add(p1);
-        list.add(p2);
-        list.add(p3);
-        list.add(p4);
-        Board b = new Board(2,2,list);
-        System.out.println(b.gameResult());
-    }
+	public static void main(String[] args) {
+		Pebble p = new Pebble(1, 1, Color.WHITE);
+		Pebble p1 = new Pebble(1, 2, Color.BLACK);
+		Pebble p2 = new Pebble(1, 3, Color.BLACK);
+		Pebble p3 = new Pebble(1, 0, Color.BLACK);
+		Pebble p4 = new Pebble(2, 6, Color.BLACK);
+		List<Pebble> list = new ArrayList<>();
+		list.add(p);
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+		list.add(p4);
+		Board b = new Board(2, 2, list);
+		System.out.println(b.gameResult());
+	}
 
 }
